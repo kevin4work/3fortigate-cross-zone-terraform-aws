de4531b981e7fa199c25e48ca4487b34e2716ae4
kevin4work <ngkamanhk@gmail.com>
2026-04-28 09:57:03 +0800

    add ak sk provider

--- provider.tf
+++ provider.tf
@@ -10,3 +10,9 @@ provider "aws" {
   region     = var.region
   profile    = "fortinet-admin"
 }
+
+//provider "aws" {
+//  region     = var.region
+//  access_key = var.access_key
+//  secret_key = var.secret_key
+//}
